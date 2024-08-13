8b202ba200089e922136f55bbdc793e6bc582457
adel-bouras <eyyad.kareem1212@gmail.com>
2024-08-13 22:57:07 +0100

    anyth

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const server = createServer(app);
 
 const io = new Server(server ,{
     cors : {
-        origin : 'http://localhost:5173',
+        origin : 'https://adelchattapp.netlify.app',
     }
 });
 
